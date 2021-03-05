404206b387fa741966264d550906d82af4b62100
lexxsage <alexdevyatovskaya@gmail.com>
2021-03-05 16:48:02 +0700

    task 2 done

--- src/Task2/GLMesh.cpp
+++
@@ -1,8 +0,0 @@
-#include "GLMesh.h"
-
-#include <utility>
-
-GLMesh::GLMesh(std::vector<GLVertex> v, std::vector<unsigned> i)
-	: vertices{std::move(v)}, indices{std::move(i)}
-{
-}
--- src/Task2/GLVertex.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <QVector3D>
-
-struct GLVertex
-{
-	QVector3D coordinate;
-	QVector3D color;
-};
